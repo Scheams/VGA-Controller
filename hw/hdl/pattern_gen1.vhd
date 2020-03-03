c1ef783cc9773b7e840c494a8c7d936db7fe1bac
Christoph Amon <christoph.amon@gmail.com>
2020-03-03 09:18:58 +0100

    First construct of Pattern Generator 1

--- hw/hdl/pattern_gen1.vhd
+++ hw/hdl/pattern_gen1.vhd
@@ -3,6 +3,12 @@ use ieee.std_logic_1164.all;
 
 entity pattern_gen1 is
   port (
-    unknown : inout std_logic
+    v_px_i  : in  std_logic_vector (9 downto 0);
+    h_px_i  : in  std_logic_vector (9 downto 0);
+    out_i   : in  std_logic;
+
+    red_o   : out std_logic_vector (3 downto 0);
+    green_o : out std_logic_vector (3 downto 0);
+    blue_o  : out std_logic_vector (3 downto 0)
   );
 end entity pattern_gen1;
--- hw/hdl/pattern_gen1_rtl.vhd
+++ hw/hdl/pattern_gen1_rtl.vhd
@@ -1,7 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 architecture rtl of pattern_gen1 is
 
 begin
 
+  p_process : process(out_i, v_px_i)
+  begin
+    red_o   <= "0000";
+    green_o <= "0000";
+    blue_o  <= "0000";
 
+    if out_i = '1' then
+      case to_integer((unsigned(v_px_i) mod 160) / 40) is
+        when 0 => red_o   <= "1111";
+        when 1 => green_o <= "1111";
+        when 2 => blue_o  <= "1111";
+        when 3 =>
+          red_o   <= "1111";
+          green_o <= "1111";
+          blue_o  <= "1111";
+        when others =>
+          red_o   <= "0000";
+          green_o <= "0000";
+          blue_o  <= "0000";
+      end case;
+    end if;
+  end process p_process;
 
 end architecture rtl;
